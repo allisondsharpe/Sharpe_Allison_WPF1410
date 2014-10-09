12fc8ff4498c160d412640928e0eb2eee9365bf8
allison sharpe <allisonsharpe@ymail.com>
2014-10-09 16:45:43 -0400

    Prompt 2: Year of Birth

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -7,3 +7,6 @@ Expressions_Personal
 var userName = prompt("What is your first and last name?");
 console.log(userName);
 
+var yearBorn = prompt("What year were you born?");
+console.log(yearBorn);
+
