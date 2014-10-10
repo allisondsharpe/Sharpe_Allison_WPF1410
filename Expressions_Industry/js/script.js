8d1eea9274f273266e0edcc51123b3ee6183820b
allison sharpe <allisonsharpe@ymail.com>
2014-10-09 21:23:04 -0400

    Arithmetic Operation

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,9 +4,16 @@ Allison Sharpe
 Expressions_Industry
  */
 
+var width = 10.0;
+var height = 13.3;
+var dotsPerInch= 96;
+var pixelsSquared = width * height/96;
+console.log(pixelsSquared);
+
 var pixelsSquared = [10.0 , 13.3 , 96];
 
 pixelsSquared [0];
 pixelsSquared [1];
 pixelsSquared [2];
 console.log(pixelsSquared);
+
