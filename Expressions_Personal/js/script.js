9bbc131ded459546a69541feffb22bce37131356
allison sharpe <allisonsharpe@ymail.com>
2014-10-09 20:42:06 -0400

    Final

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -12,9 +12,11 @@ console.log(userInput);
 var width = 15; //This is the width of my dining room
 var length = 12; //This is the length of my dining room
 var height = 8; //This is the height of my dining room
-
-var area = prompt("What is 180 sq.ft. x 8sq.ft.");//This will prompt the user to enter the complete area for dining room
+var area = 15 * 12 * 8;
 console.log(area);
+
+var result = prompt("What is 180 sq.ft. x 8sq.ft.");//This will prompt the user to enter the complete area for dining room
+console.log(result);
 
 alert("The area of my dining room is 1,440 sq.ft.");
 console.log(alert);//This will prompt the user that the area in total is 1,440sq.ft.
