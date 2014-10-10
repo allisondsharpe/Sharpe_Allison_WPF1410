b84d0824d1a4111cf9ef2679f0d47709490ad041
allison sharpe <allisonsharpe@ymail.com>
2014-10-09 22:32:11 -0400

    Arithmetic Operations

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,9 +4,26 @@ Allison Sharpe
 Expressions_Wacky
  */
 
-var personOne = 5;
-var personTwo = 2;
-var personThree = 3;
-var amount = 5 + 2 + 3;
+//Person one has five milk duds, person two has two bags of skittles, and person three has three packs of airheads.
+//I am calculating these amounts to find the total amount of candy
+
+var personOne = 5;//The total amount for person one
+var personTwo = 2;//The total amount for person two
+var personThree = 3;//The total amount for person three
+var amount = 5 + 2 + 3;//The total amount of candy added together for all three people
 console.log(amount);
 
+var total = prompt("If I added 5 boxes of milk duds, 2 bags of skittles, and 3 packs of airheads, what will be my total amount?");
+console.log(total);
+
+//Store one has 15 bars of hershey's, store two has 20 boxes of skittles, and store three has 25 things of suckers.
+//I am calculating these amounts to find the total amount of candy
+
+var storeOne = 15;
+var storeTwo = 20;
+var storeThree = 25;
+var addedAmount = 15 + 20 + 25;
+console.log(addedAmount);
+
+var amountOfCandy = prompt("If I added 15 bars of hershey's, 20 bags of skittles, and 25 things of suckers, what will be my total amount?");
+console.log(amountOfCandy);
