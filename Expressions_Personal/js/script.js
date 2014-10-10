6f0a27214b3b9116d03e0cffbaa5fa05588af7fa
allison sharpe <allisonsharpe@ymail.com>
2014-10-09 20:30:16 -0400

    Prompt and Variables

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,10 +4,16 @@ Allison Sharpe
 Expressions_Personal
  */
 
-var userInput = prompt("What is 15sq.ft. x 12sq.ft.");
+//I am measuring the width, height, and length of my dining room in order to find the area.
+
+var userInput = prompt("What is 15sq.ft. x 12sq.ft."); //This will prompt the user to enter the product for 15 x 12
 console.log(userInput);
 
-var width = 15;
-var length = 12;
-var height = 8;
+var width = 15; //This is the width of my dining room
+var length = 12; //This is the length of my dining room
+var height = 8; //This is the height of my dining room
+
+
+
+
 
