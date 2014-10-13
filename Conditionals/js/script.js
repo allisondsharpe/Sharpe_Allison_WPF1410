37a67e34e7a240cd285cc0f4ed458257fc39dc04
allison sharpe <allisonsharpe@ymail.com>
2014-10-13 14:35:41 -0400

    Conditional Example
    And operator

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,18 +1,13 @@
-//Conditional Logic - Else If
+//Conditional Logic - Logical Operators
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
 
-//if the child is old enough, print to the console "you can ride!"
-//if kid is over 48 inches in height
-if(kidHeight > minHeight){
-   //you can ride!
-   console.log("You can ride, but only with a parent present.");
-}else if(kidHeight > wParentHeight){
-    //you can ride with a parent present
+//if the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget && paycheck > 300){
+    console.log("We can buy the phone!");
+
 }else{
-    //sorry you have growing to do
-    console.log("Sorry kid, you have some growing to do first!");
-
+    console.log("No phone for you!!");
 }
