ace5ddd87ce00f40cbbd2bd6d7b083739df35540
allison sharpe <allisonsharpe@ymail.com>
2014-10-13 13:25:55 -0400

    Conditionals Example
    False

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -1,6 +1,6 @@
 //Conditional Logic
 
-var oldEnough = true;
+var oldEnough = false;
 
 //if the child is old enough, print to the console "you can ride!"
 
@@ -8,3 +8,5 @@ if(oldEnough){
    //code performed if condition is true
    console.log("You can ride the coaster!");
 }
+console.log("what comes after");
+
