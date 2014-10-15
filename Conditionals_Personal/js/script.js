2b860da05fcedb3f6d8c926fcbb78a55f742d72b
allison sharpe <allisonsharpe@ymail.com>
2014-10-15 12:57:46 -0400

    Conditionals_Personal
    Prompt and prompt validation

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -12,5 +12,11 @@ var height = 17;//The height of my bedroom is 17 ft.
 var area = width * height;//The area will be width times height
 console.log(area);
 
+var calculatedArea = prompt("What is the area of my bedroom if the width is 14 sq.ft. and the height is 17sq.ft.?");
+console.log(calculatedArea);
+
+if(calculatedArea === "");
+calculatedArea = prompt("Please enter the area for 14sq.ft. and 17sq.ft.");
+
 
 
