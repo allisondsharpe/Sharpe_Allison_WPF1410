06fb3ea95ad51c4dcfb69365a3c42651dd923920
allison sharpe <allisonsharpe@ymail.com>
2014-10-15 12:49:02 -0400

    Conditionals_Personal
    Variable solution 1

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,6 +4,13 @@ Allison Sharpe
 Conditionals_Personal
  */
 
+//I am thinking of a solution that calculates the measurements of my bedroom
+
+
+var width = 14;//The width of my bedroom is 14 ft.
+var height = 17;//The height of my bedroom is 17 ft.
+var area = width * height;//The area will be width times height
+console.log(area);
 
 
 
