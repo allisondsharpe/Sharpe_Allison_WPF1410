dff057cc3818909783056d5fa4a20165f259c1cf
allison sharpe <allisonsharpe@ymail.com>
2014-10-16 13:09:35 -0400

    Conditionals_Wacky
    Prompt 1

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,6 +12,9 @@ var Andrea = 3;
 var totalAmount = (5 + 7 + 3);
 console.log(totalAmount);
 
+var marbles = prompt("Daryl has 5 marbles, Rick has 7 marbles, and Andrea has 3 marbles. How many do they have in total?");
+console.log(marbles);
+
 if(totalAmount < 10){
     console.log("Buy or find more marbles.");
 }else{
