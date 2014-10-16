733d5858b88a9b70c6622231cd30870a7632f349
allison sharpe <allisonsharpe@ymail.com>
2014-10-16 13:09:08 -0400

    Conditionals_Wacky
    If and else statements

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,11 +4,18 @@ Allison Sharpe
 Conditionals_Wacky
  */
 
-//If Daryl has 5 marbles, Rick has 7 marbles, and Andrea has 3 marbles, how many marbles do they all have in total?
+//If Daryl has 5 marbles, Rick has 7 marbles, and Andrea has 3 marbles. How many marbles do they all have in total?
 
 var Daryl = 5;
 var Rick = 7;
 var Andrea = 3;
 var totalAmount = (5 + 7 + 3);
 console.log(totalAmount);
+
+if(totalAmount < 10){
+    console.log("Buy or find more marbles.");
+}else{
+   console.log("Play a game consisting of marbles.");
+}
+
 
