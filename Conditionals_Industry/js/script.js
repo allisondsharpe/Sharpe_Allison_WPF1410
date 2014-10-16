9ba87a8c1cc4dc7e816816734b199567ca251530
allison sharpe <allisonsharpe@ymail.com>
2014-10-16 12:24:05 -0400

    Conditionals_Industry
    Variables: Width x Height

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -4,3 +4,10 @@ Allison Sharpe
 Conditionals_Industry
  */
 
+//I am measuring the width and height for coding a website to adjust to a mobile user's screen resolution
+
+var width = 640;//Calculating the width of the user's mobile device's screen resolution
+var height = 960;//Calculating the height of the user's mobile device's screen resolution
+var area = width * height;//Multiplying width and height in order to find the area
+console.log(area);//This will tell the console to generate the area for the width and height
+
