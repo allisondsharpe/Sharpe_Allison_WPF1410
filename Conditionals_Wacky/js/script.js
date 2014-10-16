9b96e489fcf929580061c09f5412278abdcd33df
allison sharpe <allisonsharpe@ymail.com>
2014-10-16 13:15:05 -0400

    Conditionals_Wacky
    Validation prompt

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -9,16 +9,18 @@ Conditionals_Wacky
 var Daryl = 5;
 var Rick = 7;
 var Andrea = 3;
-var totalAmount = (5 + 7 + 3);
+var totalAmount = (5 + 7 + 3);//The total amount will be all three variables added together
 console.log(totalAmount);
 
 var marbles = prompt("Daryl has 5 marbles, Rick has 7 marbles, and Andrea has 3 marbles. How many do they have in total?");
-console.log(marbles);
+console.log(marbles);//This will prompt the user to add all variables to find the total amount of marbles
+
+if(marbles === "");//This is a validation prompt that will enable the user to enter in total amount for the second time
+marbles = prompt("Daryl has 5 marbles, Rick has 7 marbles, and Andrea has 3 marbles. How many do they have in total? Please don't leave blank.");
 
 if(totalAmount < 10){
     console.log("Buy or find more marbles.");
 }else{
    console.log("Play a game consisting of marbles.");
-}
-
+}//This is the "if and else" operator that will enable the console to directing the user as to what they should do next
 
