2193f20d2ec619b2ef142dc2b7a4271c138840d1
allison sharpe <allisonsharpe@ymail.com>
2014-10-20 16:03:52 -0400

    Loops
    Do while loops

--- Loops/js/script.js
+++ Loops/js/script.js
@@ -1,9 +1,11 @@
 //Loops
 
-var b = 10; //sets up index
+var c = 10; //sets up index
 
-while(b > 0){ //checks the condition
-    console.log(b + ' kegs on the wall ');
-    b--; //increments or decrements index 
+do{
+    console.log(c + ' kegs on the wall ');
+    c--;
 }
+
+while(c > 0);
 
