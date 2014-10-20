5408f67d0b0b3739dc33c609cb92fdad926bee88
allison sharpe <allisonsharpe@ymail.com>
2014-10-20 18:36:40 -0400

    Functions_Worksheet
    Example one revised function and return

---
+++ Functions_Worksheet/js/script.js
@@ -0,0 +1,14 @@
+/*
+Allison Sharpe
+10-20-14
+Functions_Worksheet
+ */
+
+var total = calculateArea(5, 6);
+
+function calculateArea(width, height){
+var area = width * height;//This is the calculated area for the rectangle
+console.log(" The Area of a Rectangle is " + area);//This will tell the console to
+    return area;
+}
+console.log(total);
