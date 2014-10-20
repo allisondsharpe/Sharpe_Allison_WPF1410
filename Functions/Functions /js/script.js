85c2497ddfc46382ab0d6bd677d0713057bf9d38
allison sharpe <allisonsharpe@ymail.com>
2014-10-20 14:50:35 -0400

    Functions
    Basic structure

---
+++ Functions/Functions /js/script.js
@@ -0,0 +1,14 @@
+//Functions - Basic Structure
+
+function OutptMsg(){
+    console.log("Hello world!");
+}
+
+function calcArea(){
+
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
+
