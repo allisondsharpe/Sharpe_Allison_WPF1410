956e1c418a10cc55d2ea0b2638794773401a3f7e
allison sharpe <allisonsharpe@ymail.com>
2014-10-20 19:20:28 -0400

    Functions_Worksheet
    Example two function

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -8,7 +8,16 @@ var total = calculateArea(5, 6);
 
 function calculateArea(width, height){
 var area = width * height;//This is the calculated area for the rectangle
-console.log(" The Area of a Rectangle is " + area);//This will tell the console to
+//console.log(" The Area of a Rectangle is " + area);//This will tell the console to
     return area;
 }
-console.log(total);
+//console.log(total);
+
+findingCircumference(5, 5, 3.14);
+
+function findingCircumference(radius, radius2, pie){
+    var circumference = radius * radius2 * pie;
+    console.log(" The circumference of the circle is " + circumference);
+}
+
+
