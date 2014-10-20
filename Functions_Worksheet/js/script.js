687de997002cbe341383e7184de7f7045ed7b5b9
allison sharpe <allisonsharpe@ymail.com>
2014-10-20 19:29:08 -0400

    Functions_Worksheet
    Comments for examples one and two

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -4,22 +4,33 @@ Allison Sharpe
 Functions_Worksheet
  */
 
-var total = calculateArea(5, 6);
+//Example One
 
-function calculateArea(width, height){
+var total = calculateArea(5, 6);//This is the function call for finding the area
+
+function calculateArea(width, height){//This is the function for the rectangle
 var area = width * height;//This is the calculated area for the rectangle
-//console.log(" The Area of a Rectangle is " + area);//This will tell the console to
-    return area;
+//console.log(" The Area of a Rectangle is " + area);//This will tell the console to print out the calculated area
+    return area;//This will return the values from the function
 }
-//console.log(total);
+//console.log(total);//This will enable the console to show the returned values
 
-findingCircumference(5, 5, 3.14);
 
-function findingCircumference(radius, radius2, pie){
-    var circumference = radius * radius2 * pie;
-    console.log(" The circumference of the circle is " + circumference);
+//Example Two
+
+findingCircumference(5, 5, 3.14);//This is the function call for finding circumference
+
+function findingCircumference(radius, radius2, pie){//This is the function for finding the circumference
+    var circumference = radius * radius2 * pie;//These are the calculations required to find circumference: radius x itself x pie(3.14)
+    //console.log(" The circumference of the circle is " + circumference);//This will tell the console to print out the circumference
+    return circumference;//This will return the values from the function
 }
-console.log(findingCircumference);
+//console.log(findingCircumference);//This will enable the console to show the returned values
+
+
+//Example Three
+
+
 
 
 
