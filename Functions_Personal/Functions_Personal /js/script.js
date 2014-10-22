63e5547c2d69f87b760233ca6435ec7ce2cbdcc6
allison sharpe <allisonsharpe@ymail.com>
2014-10-22 19:43:25 -0400

    Functions_Personal
    Prompt & while loop

--- Functions_Personal/Functions_Personal /js/script.js
+++ Functions_Personal/Functions_Personal /js/script.js
@@ -7,10 +7,18 @@ Functions_Personal
 alert("We are finding the area of my kitchen!");
 console.log(alert);
 
+var areaPrompt = prompt("What is the area of my kitchen if the width is 180ft. and the height is 170ft.?");
+while(areaPrompt===""){
+    areaPrompt = prompt("Please enter the area for my kitchen if the width is 180ft. and the height is 170ft.?");
+    console.log(prompt);
+}
+
 var totalArea = function(width, height){
     var area = width * height;
+    console.log(" The area of my kitchen is " + area + " ft.");
     return area;
 }
 
 var a = totalArea(180, 170);
 console.log(a);
+
