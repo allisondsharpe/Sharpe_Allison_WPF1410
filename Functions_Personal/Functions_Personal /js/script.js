6f12e01152d32bd8e2028957e90a5e2d270712b5
allison sharpe <allisonsharpe@ymail.com>
2014-10-22 19:28:24 -0400

    Functions_Personal
    Anonymous function operator

--- Functions_Personal/Functions_Personal /js/script.js
+++ Functions_Personal/Functions_Personal /js/script.js
@@ -5,4 +5,12 @@ Functions_Personal
  */
 
 alert("We are finding the area of my kitchen!");
+console.log(alert);
 
+var totalArea = function(width, height){
+    var area = width * height;
+    return area;
+}
+
+var a = totalArea(180, 170);
+console.log(a);
