95c93a73bc781657ff0ad172cb728d17738ed115
allison sharpe <allisonsharpe@ymail.com>
2014-10-23 13:50:13 -0400

    Functions_Industry
    Function and return

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -6,3 +6,14 @@ Functions_Industry
 
 alert("We are measuring the width x height of a screen resolution!");
 console.log(alert);
+
+var total = resolution(1440, 900);
+
+    function resolution(width, height){
+        var area = width * height;
+        return area;
+
+    }
+console.log (total);
+
+
