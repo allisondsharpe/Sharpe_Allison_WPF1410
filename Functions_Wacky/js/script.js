8e298086d40a470d5ef2cf64722fed4371d5212b
allison sharpe <allisonsharpe@ymail.com>
2014-10-23 15:11:39 -0400

    Functions_Wacky
    Function with four parameters

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -11,3 +11,17 @@ var fruit = [2,3,6,9];
 var total = fruit[0] + fruit[1] + fruit[2] + fruit[3];
 console.log(total);
 
+var amount = totalFruit(2,3,6,9);
+
+function totalFruit(alice, sharon, chris, caleb){
+    var amount = alice + sharon + chris + caleb;
+    return amount;
+
+}
+console.log(" The total amount of fruit is " + amount);
+
+
+
+
+
+
