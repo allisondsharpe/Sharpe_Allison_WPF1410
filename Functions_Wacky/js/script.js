c8152f815d54ca5ce6cc0fe89ad15d3043c9f38e
allison sharpe <allisonsharpe@ymail.com>
2014-10-23 15:04:13 -0400

    Functions_Wacky
    Array

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,5 +7,7 @@ Functions_Wacky
 alert("We are calculating the total amount of fruit!");
 console.log(alert);
 
-
+var fruit = [2,3,6,9];
+var total = fruit[0] + fruit[1] + fruit[2] + fruit[3];
+console.log(total);
 
